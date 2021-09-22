a02470bdb1c2c83a094a90ba8a9355a994a83b14
KD <59188880+KD1903@users.noreply.github.com>
2021-09-22 11:22:59 +0530

    Update settings.py

--- stockAnalysis/settings.py
+++ stockAnalysis/settings.py
@@ -32,8 +32,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dp312444@gmail.com'
-EMAIL_HOST_PASSWORD = 'ThopTV@@30'
+EMAIL_HOST_USER = 'dp******@gmail.com'
+EMAIL_HOST_PASSWORD = '**********'
 
 # Application definition
 
